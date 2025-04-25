b5d38d21f263f5d693a578950c8e58729b455d3b
Cysteine12 <gbengapeace2001@gmail.com>
2025-04-25 18:04:19 +0100

    fix(carousel): use import syntax

--- src/components/AppSlider.jsx
+++ src/components/AppSlider.jsx
@@ -1,3 +1,8 @@
+import Slider1 from '@/assets/imgs/Pemike_1.1.jpg'
+import Slider2 from '@/assets/imgs/Pemike_2.1.jpg'
+import Slider3 from '@/assets/imgs/Pemike_3.1.jpg'
+import Slider4 from '@/assets/imgs/Pemike_4.1.jpg'
+import Slider5 from '@/assets/imgs/Pemike_5.1.jpg'
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
@@ -28,39 +33,19 @@ export default function AppSlider() {
     <div className="mt-8 mx-auto px-16">
       <Slider {...settings}>
         <div className="max-w-96">
-          <img
-            src="../src/assets/imgs/Pemike_1.1.jpg"
-            alt="Slide 1"
-            className="rounded-2xl"
-          />
+          <img src={Slider1} alt="Slide 1" className="rounded-2xl" />
         </div>
         <div className="max-w-96">
-          <img
-            src="../src/assets/imgs/Pemike_2.1.jpg"
-            alt="Slide 2"
-            className="rounded-2xl"
-          />
+          <img src={Slider2} alt="Slide 2" className="rounded-2xl" />
         </div>
         <div className="max-w-96">
-          <img
-            src="../src/assets/imgs/Pemike_3.1.jpg"
-            alt="Slide 3"
-            className="rounded-2xl"
-          />
+          <img src={Slider3} alt="Slide 3" className="rounded-2xl" />
         </div>
         <div className="max-w-96">
-          <img
-            src="../src/assets/imgs/Pemike_4.1.jpg"
-            alt="Slide 4"
-            className="rounded-2xl"
-          />
+          <img src={Slider4} alt="Slide 4" className="rounded-2xl" />
         </div>
         <div className="max-w-96">
-          <img
-            src="../src/assets/imgs/Pemike_5.1.jpg"
-            alt="Slide 4"
-            className="rounded-2xl"
-          />
+          <img src={Slider5} alt="Slide 5" className="rounded-2xl" />
         </div>
       </Slider>
     </div>
